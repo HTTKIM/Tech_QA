65171de0640e0d480dbda17d4a908d56c1aa1244
HTTKIM <151608618+HTTKIM@users.noreply.github.com>
2024-11-21 16:23:21 +0900

    Update QA_RAG.py

--- QA_RAG.py
+++ QA_RAG.py
@@ -8,6 +8,9 @@ from langchain.callbacks.base import BaseCallbackHandler
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 
+from langchain_community.document_transformers import LongContextReorder
+from langchain.retrievers.multi_query import MultiQueryRetriever
+
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -39,7 +42,15 @@ db = FAISS.load_local(
     allow_dangerous_deserialization=True
 )
 
-retriever = db.as_retriever(search_type="similarity", search_kwargs={'k': 5, 'fetch_k': 20})
+########## LLM & Retriever ##########
+
+llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0.2, streaming=True)
+
+long_context_reorder = LongContextReorder()
+
+retriever = db.as_retriever(search_type="similarity", search_kwargs={'k': 5, 'fetch_k': 20}, document_transformer = long_context_reorder)
+
+multiquery_retriever = MultiQueryRetriever.from_llm(retriever = retriever, llm = llm)
 
 def format_docs(docs):
     return "\n\n".join([doc.page_content for doc in docs])
@@ -47,7 +58,10 @@ def format_docs(docs):
 ############################################## RAG ########################################################
 
 ########## Creating prompt ##########
-prompt_template = """You are an expert in fire safety, evacuation, and fire prevention. The person asking questions is also a fire safety, evacuation, and fire prevention professional with basic knowledge. Respond based on the relevant regulations provided by the user. You may use line breaks or numbering in your explanation to make the content easier to read. Include detailed information in your response without summarizing, ensuring that no essential content is omitted. If you don't know the answer, just say that you don't know, don't try to make up an answer. Answer in Korean. your name is Bridge 기술 챗봇.
+prompt_template = """You are an expert in fire safety, evacuation, and fire prevention. The person asking questions is also a fire safety, evacuation, and fire prevention professional with basic knowledge.
+                     Respond based on the relevant regulations provided by the user. You may use line breaks or numbering in your explanation to make the content easier to read.
+                     Include detailed information in your response without summarizing, ensuring that no essential content is omitted. If you don't know the answer, just say that you don't know, don't try to make up an answer. Answer in Korean.
+                     Do not add unnecessary comments; focus on technical content.
 
 {context}
 
@@ -57,13 +71,11 @@ Helpful Answer:
 
 prompt = PromptTemplate(template=prompt_template, input_variables=['context', 'question'])
 
-########## Creating prompt ##########
 
-llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0.2, streaming=True)
 
 ########## use when using RetrievalQA chain from llm's chain ##########
 qa = RetrievalQA.from_chain_type(llm=llm, chain_type='stuff',
-                                 retriever=retriever,
+                                 retriever=Multiquery_retriever,
                                  return_source_documents=True,
                                  chain_type_kwargs={'prompt': prompt},
                                  verbose=False)
