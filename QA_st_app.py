e3a535f32a025c650d703a794acb1f12dc6fe095
HTTKIM <151608618+HTTKIM@users.noreply.github.com>
2024-12-17 09:52:26 +0900

    Update QA_st_app.py

--- QA_st_app.py
+++ QA_st_app.py
@@ -75,7 +75,8 @@ st.markdown(
 
 st.markdown('<div class="custom-list">좌측 사이드바에서 DATA를 선택해 주세요</div>', unsafe_allow_html=True)
 st.markdown('<div class="custom-list">[키워드] 기반 법령 내용을 검색하는 목적입니다. 법령에 있는 단어 검색 용도로 사용하세요</div>', unsafe_allow_html=True)
-st.markdown('<div class="custom-sub-list">예시) EPS실, 피트실 등 법령에 없는 단어는 답변이 불가합니다. 방화구획, 피난계단, 옥내소화전 등 법령에 있는 단어는 검색 가능', unsafe_allow_html=True)
+st.markdown('<div class="custom-sub-list">예시) EPS실, 피트실 등 법령에 없는 단어는 답변이 불가합니다.', unsafe_allow_html=True)
+st.markdown('<div class="custom-sub-list">예시) 방화구획, 피난계단, 옥내소화전 등 법령에 있는 단어는 검색 가능', unsafe_allow_html=True)
 
 ################ Sidebar 설정 ################
 
