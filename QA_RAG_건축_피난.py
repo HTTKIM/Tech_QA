9a630dae8c9276c7ba3bb679ecf774e9350b31fb
HTTKIM <151608618+HTTKIM@users.noreply.github.com>
2024-12-17 09:13:12 +0900

    Update QA_RAG_건축_피난.py

--- QA_RAG_건축_피난.py
+++ QA_RAG_건축_피난.py
@@ -63,7 +63,7 @@ def initialize_rag():
 
     if db is None:
         db = FAISS.load_local(
-            folder_path="C:\\python_rag\\database\\DB(241216)_arch",
+            folder_path="DB(241216)_arch",
             embeddings=embedding,
             allow_dangerous_deserialization=True
         )
